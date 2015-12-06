bba2e429a378fa67045d7a36a5826d694baa8c05
Joel Klabo <joelklabo@gmail.com>
2015-12-06 09:48:59 -0800

    Adding test for too many satoshis

--- server.py
+++ server.py
@@ -3,11 +3,14 @@
 from flask import Flask, jsonify
 from flask import request
 
+from invalid_usage import InvalidUsage
+
 # Import from the 21 Bitcoin Develper Library
 #from two1.lib.wallet import Wallet
 #from two1.lib.bitserv.flask import Payment
 
 PORT = 5000
+MAX_RISK_AMOUNT = 10000
 
 # Configure the app
 app = Flask(__name__)
@@ -16,7 +19,18 @@ app = Flask(__name__)
 
 @app.route('/risk/<int:risk_amount>')
 def risk(risk_amount):
-  return '{:d}'.format(risk_amount) 
+
+  if risk_amount > MAX_RISK_AMOUNT:
+    error_string = 'Risk amount must be less than {:d}'.format(MAX_RISK_AMOUNT)
+    raise InvalidUsage(error_string, status_code=400)
+
+  return '{:d}'.format(risk_amount)
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+  response = jsonify(error.to_dict())
+  response.status_code = error.status_code
+  return response
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=PORT)
--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ import server
 import unittest
 import tempfile
 
-class FlaskrTestCase(unittest.TestCase):
+class RiskTestCase(unittest.TestCase):
 
   def setUp(self):
     server.app.config['TESTING'] = True
@@ -13,6 +13,12 @@ class FlaskrTestCase(unittest.TestCase):
     response = self.app.get('/risk/123')
     response_string = response.get_data().decode('utf-8')
     assert response_string == '123'
+    
+  def test_risk_endpoint_too_high(self):
+    try:
+      response = self.app.get('/risk/1001')
+    except Exception:
+      assert True
 
 if __name__ == '__main__':
   unittest.main()
