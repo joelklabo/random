e94a075cf18a2e3075619f5b5d314d7718bc3d2b
Joel Klabo <joelklabo@gmail.com>
2015-12-06 17:42:37 -0800

    Doing payment handshake manually

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify
+from flask import Flask, Response 
 from flask import request
 from risk_engine import RiskEngine
 from invalid_usage import InvalidUsage
@@ -8,7 +8,9 @@ from invalid_usage import InvalidUsage
 # Import from the 21 Bitcoin Develper Library
 from two1.lib.wallet import Wallet
 from two1.lib.bitserv.flask import Payment
-from two1tools.two1tools.bittransfer import *
+from two1.commands.config import Config
+
+from two1.lib.util import zerotier 
 
 PORT = 5001
 MAX_RISK_AMOUNT = 1000
@@ -22,7 +24,12 @@ payment = Payment(app, wallet)
 @app.route('/risk/<int:risk_amount>')
 def risk(risk_amount):
 
-  username = request.args.get('username')
+  if transfer = bitcoin_transfer_dict(request):
+    # The user has made the payment request
+    print(transfer)
+  else:
+    # Return a 402 so user can request with payment
+    return payment_required_response(risk_amount)
 
   if risk_amount > MAX_RISK_AMOUNT:
     error_string = 'Risk amount must be less than {:d}'.format(MAX_RISK_AMOUNT)
@@ -40,6 +47,22 @@ def risk(risk_amount):
   else:
     # Client lost, return message
     return loser_message(username, risk_amount)
+
+def payment_required_response(amount):
+  resp = Response('Payment Required')
+  resp.status_code = 402
+  resp.headers['Bitcoin-Micropayment-Server'] = payment_server_address() 
+  resp.headers['Username'] = Config().username
+  resp.headers['Price'] = amount
+  resp.headers['Bitcoin-Address'] = wallet.get_payout_address() 
+  print(resp.headers)
+  return resp
+
+def payment_server_address():
+  return 'http://{0}:{0}/payment'.format(zerotier.device_ip(), PORT) 
+
+def bitcoin_transfer_dict(request):
+  transfer = request.headers['Bitcoin-Transfer']
 
 def winner_message(user, reward):
   return 'Congratulations {0} you won {0} satoshis!'.format(user, reward)
